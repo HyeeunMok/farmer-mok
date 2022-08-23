ed755b006481867e4e88aebc7296645b1a72c343
Hyeeun Mok <hyeeuni.mok@gmail.com>
2022-08-23 19:47:33 -0400

    add tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 const App = () => (
-  <div>
-    <p>문학 좀 한다는 시골 농부 목씨</p>
-  </div>
+  <h1 className="text-3xl font-bold">문학 좀 한다는 시골 농부 목씨</h1>
 );
 
 export default App;
