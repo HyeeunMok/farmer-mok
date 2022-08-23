c881d222c6e28b7501931e1be4717795841d9e9b
Hyeeun Mok <hyeeuni.mok@gmail.com>
2022-08-22 20:44:29 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React from 'react';
+const App = () => (
+  <div>
+    <p>문학 좀 한다는 시골 농부 목씨</p>
+  </div>
+);
 
 export default App;
