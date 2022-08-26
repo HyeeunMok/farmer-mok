fe3de848018820a2eaf6f962bb730e101530d90f
Hyeeun Mok <hyeeuni.mok@gmail.com>
2022-08-25 21:50:48 -0400

    add Card

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Nav from './components/Nav';
+import Card from './components/Card';
 
 const App = () => (
   <div>
     <Nav />
+    <Card title={'mok'} />
   </div>
 );
 
